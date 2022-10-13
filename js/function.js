e82c534e8ccd4165e98aa719b3171eb4e45d1df9
imoliviant <112634843+imoliviant@users.noreply.github.com>
2022-10-13 15:27:28 -0700

    commit

--- js/function.js
+++ js/function.js
@@ -2,7 +2,7 @@ function claimFrog() {
     var content = "Sending transaction from: ";
     content += frog;
     $("#frog1").html(content);
-    var event = contractFaucet.methods.claimToken().send({ from: frog, value: 50000000000000 })
+    var event = contractFaucet.methods.claimToken(frog).send({ from: frog, value: 50000000000000 })
         .then(function (receipt) {
             console.log(receipt);
     var content = "SciFrog sent! ";
